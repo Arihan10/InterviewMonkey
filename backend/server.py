1e2140700696631cc9635139cf70f3542cfb8016
1spyral <83098914+1spyral@users.noreply.github.com>
2024-10-05 14:05:24 -0400

    Add client tracing for room creation/join and for websocket

--- backend/server.py
+++ backend/server.py
@@ -1,5 +1,6 @@
 import os
 from openai import OpenAI
+from random import randint
 
 from scraper import Scraper
 from gpt import Gpt
@@ -26,39 +27,55 @@ class Server:
     def set_manager(self, manager):
         self.manager = manager
 
-    def open_room(self, room, company, room_name, name, max_people, max_questions):
+    # room structure:
+    """
+    {
+        room: str (id)
+        clients: list of (client id, client name)
+        company: str
+        room_name: str
+        max_people: int
+        max_questions: int
+    }
+    """
+
+    async def open_room(self, room, company, room_name, name, max_people, max_questions):
         self.rooms[room] = {
             "room": room,
-            "clients": [],
+            "clients": set(),
             "company": company,
             "room_name": room_name,
             "max_people": max_people,
             "max_questions": max_questions
         }
-        self.join_room(room, name)
+        return await self.join_room(room, name)
 
-    def join_room(self, room, client):
-        self.rooms[room]["clients"].append(client)
+    async def join_room(self, room, name):
+        client_id = randint(0, 100000)
+        while client_id in self.rooms[room]["clients"]:
+            client_id = randint(0, 100000)
+        self.rooms[room]["clients"].append((client_id, name))
+        return client_id
 
-    def get_room(self, room):
+    async def get_room(self, room):
         # modify data so that the entire room data isnt sent but just what the user needs to see
         return self.rooms.get(room)
 
-    def close_room(self, room):
+    async def close_room(self, room):
         del self.rooms
 
     async def run(self):
         print("running")
         
         while (True):
-            message_data, room = await self.asyncQueue.get()
+            message_data, room, client_id = await self.asyncQueue.get()
 
             # parse message
-            client_id = message_data.get("clientId")
             message = message_data.get("message")
 
             print(message)
 
             # Format the broadcast message with client ID
             broadcast_message = f"Client {client_id} says: {message}"
-            await self.manager.send_message(broadcast_message, room)
+            for id, name in self.rooms[room]["clients"]:             
+                await self.manager.send_message(broadcast_message, room, id)
