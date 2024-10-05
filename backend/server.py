edd2a48a2af602213babf9b48ea1c04ead4c4cab
William Zeng <61915438+willzeng274@users.noreply.github.com>
2024-10-05 05:23:22 -0400

    added ai gen

--- backend/server.py
+++ backend/server.py
@@ -5,8 +5,6 @@ from scraper import Scraper
 from reader import Reader
 from interviewer import Interviewer
 
-
-
 class Server:
     def __init__(self):
         self.client = OpenAI(
@@ -14,7 +12,11 @@ class Server:
             organization = os.environ.get("OPENAI_ORGANIZATION")
         )
 
-        self.scraper = Scraper()
+        self.scraper = Scraper(self.client)
         self.reader = Reader(self.client)
         self.interviewer = Interviewer(self.client)
 
+if __name__ == "__main__":
+    from dotenv import load_dotenv
+
+    load_dotenv()
