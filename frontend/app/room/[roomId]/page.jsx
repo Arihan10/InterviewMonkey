149f0b37cd270a17cd8a9c17793820b78db5e878
m-shao <dev.minglunshao@gmail.com>
2024-10-05 01:56:27 -0400

    fix/frontend to next

---
+++ frontend/app/room/[roomId]/page.jsx
@@ -0,0 +1,53 @@
+"use client"
+
+import React, { useState, useEffect } from "react";
+import { useParams } from "next/navigation";
+
+const Room = () => {
+	const { roomId } = useParams();
+	const [message, setMessage] = useState("");
+	const [messages, setMessages] = useState([]);
+	let socket;
+
+	useEffect(() => {
+		// Establish WebSocket connection to FastAPI
+		socket = new WebSocket(`ws://localhost:8000/ws/${roomId}`);
+
+		// Handle incoming messages from the server
+		socket.onmessage = (event) => {
+			setMessages((prev) => [...prev, event.data]);
+		};
+
+		// Clean up WebSocket connection on component unmount
+		return () => {
+			socket.close();
+		};
+	}, [roomId]);
+
+	const handleSendMessage = () => {
+		if (socket) {
+			socket.send(message);
+			setMessage(""); // Clear the message input after sending
+		}
+	};
+
+	return (
+		<div>
+			<h2>Room: {roomId}</h2>
+			<div>
+				{messages.map((msg, index) => (
+					<p key={index}>{msg}</p>
+				))}
+			</div>
+			<input
+				type='text'
+				value={message}
+				onChange={(e) => setMessage(e.target.value)}
+				placeholder='Type a message'
+			/>
+			<button onClick={handleSendMessage}>Send</button>
+		</div>
+	);
+};
+
+export default Room;
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,12 +1,17 @@
 /** @type {import('tailwindcss').Config} */
-export default {
+module.exports = {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        background: "var(--background)",
+        foreground: "var(--foreground)",
+      },
+    },
   },
   plugins: [],
-}
-
+};
