0dd5793dee3f9ad57fcf7cb444a08bdf944d3488
sofwarearihan@gmail.com <sofwarearihan@gmail.com>
2024-10-05 05:36:41 -0400

    move message handling to server.py
    Co-authored-by: Luke Zhan <luke.zhan@uwaterloo.ca>

--- backend/server.py
+++ backend/server.py
@@ -1,23 +1,40 @@
 import os
 from openai import OpenAI
 
-from scraper import Scraper
-from reader import Reader
-from interviewer import Interviewer
+# from scraper import Scraper
+# from reader import Reader
+# from interviewer import Interviewer
 
+import asyncio
 
 
 class Server:
-    def __init__(self):
-        self.client = OpenAI(
-            api_key = os.environ.get("OPENAI_API_KEY"),
-            organization = os.environ.get("OPENAI_ORGANIZATION")
-        )
+    def __init__(self, asyncQueue: asyncio.Queue, manager):
+        self.asyncQueue = asyncQueue
+        self.manager = manager
 
-        self.scraper = Scraper()
-        self.reader = Reader(self.client)
-        self.interviewer = Interviewer(self.client)
+        # self.client = OpenAI(
+        #     api_key = os.environ.get("OPENAI_API_KEY"),
+        #     organization = os.environ.get("OPENAI_ORGANIZATION")
+        # )
+
+        # self.scraper = Scraper()
+        # self.reader = Reader(self.client)
+        # self.interviewer = Interviewer(self.client)
 
 
-    def run(self, message, room):
-        pass
+    async def run(self):
+        print("running")
+        
+        while (True):
+            message_data, room = await self.asyncQueue.get()
+
+            # parse message
+            client_id = message_data.get("clientId")
+            message = message_data.get("message")
+
+            print(message)
+
+            # Format the broadcast message with client ID
+            broadcast_message = f"Client {client_id} says: {message}"
+            await self.manager.send_message(broadcast_message, room)
